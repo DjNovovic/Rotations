20a18b543037d8c51755ded8887d65aa2e38708a
DjNovovic <djordjijenovovic@gmail.com>
2021-09-19 13:34:31 +0200

    Initial Commit

---
+++ Rotations/RotationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationsApp.swift
+//  Rotations
+//
+//  Created by Đorđije Novović on 19.9.21..
+//
+
+import SwiftUI
+
+@main
+struct RotationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
